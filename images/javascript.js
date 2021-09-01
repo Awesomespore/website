ff25ac7991d61ec11cf4a18bdcb2cfa41f308eac
Awesomespore <59407684+Awesomespore@users.noreply.github.com>
2021-09-01 15:05:46 +1200

    Delete images directory

--- images/javascript.js
+++
@@ -1,5 +0,0 @@
-function showCodeBlock() {
-	// Show code block and remove the button
-	document.getElementById("code_block").style.display = "block";
-	document.getElementById("code_button").style.display = "none";
-}
