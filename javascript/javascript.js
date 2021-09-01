11c165d00a4ec28e03f4e7b00fadda1c08c9219e
Awesomespore <59407684+Awesomespore@users.noreply.github.com>
2021-09-01 15:10:12 +1200

    Create javascript.js

---
+++ javascript/javascript.js
@@ -0,0 +1,5 @@
+function showCodeBlock() {
+	// Show code block and remove the button
+	document.getElementById("code_block").style.display = "block";
+	document.getElementById("code_button").style.display = "none";
+}
